695c0197483ed5d3bb576abf84338145ff78ff7e
patthad <beebobbob956@gmai.com>
2025-03-04 10:32:53 +0700

    feat: 🎸 add lizard spock logic

--- RPS.sol
+++ RPS.sol
@@ -6,7 +6,7 @@ pragma solidity >=0.7.0 <0.9.0;
 contract RPS {
     uint public numPlayer = 0;
     uint public reward = 0;
-    mapping (address => uint) public player_choice; // 0 - Rock, 1 - Paper , 2 - Scissors
+    mapping (address => uint) public player_choice; // 0 - Scissors, 1 - Paper , 2 - Rock, 3 - Lizard, 4 - Spock
     mapping(address => bool) public player_not_played;
     address[] public players;
 	address[] public allowPlayers = [0x5B38Da6a701c568545dCfcB03FcB875f56beddC4, 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2,0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db,0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB];
@@ -31,7 +31,7 @@ contract RPS {
     function input(uint choice) public  {
         require(numPlayer == 2);
         require(player_not_played[msg.sender]);
-        require(choice == 0 || choice == 1 || choice == 2);
+        require(choice == 0 || choice == 1 || choice == 2 || choice == 3 || choice == 4);
         player_choice[msg.sender] = choice;
         player_not_played[msg.sender] = false;
         numInput++;
@@ -45,13 +45,13 @@ contract RPS {
         uint p1Choice = player_choice[players[1]];
         address payable account0 = payable(players[0]);
         address payable account1 = payable(players[1]);
-        if ((p0Choice + 1) % 3 == p1Choice) {
+        if ((p0Choice + 1) % 3 == p1Choice || (p0Choice + 3) % 5 == p1Choice) {
             // to pay player[1]
-            account1.transfer(reward);
+            account0.transfer(reward);
         }
-        else if ((p1Choice + 1) % 3 == p0Choice) {
+        else if ((p1Choice + 1) % 3 == p0Choice || (p1Choice + 3) % 5 == p0Choice) {
             // to pay player[0]
-            account0.transfer(reward);    
+            account1.transfer(reward);    
         }
         else {
             // to split reward
